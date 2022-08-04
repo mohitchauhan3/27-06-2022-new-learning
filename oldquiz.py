d0d4aff0c3c54418a9783e4d26e6bb6667ed81c2
Mohit Chauhan <101303720+mohitchauhan3@users.noreply.github.com>
2022-08-04 21:28:10 +0000

    Initial commit

---
+++ oldquiz.py
@@ -0,0 +1,90 @@
+#4.1
+#10/06/2022
+#Mohit Chauhan
+
+print("Kia Ora Ko Wai Tou Ingoa? / meaning Hello What is your name?")
+n = input()
+print("Kia Ora / hello " + n )
+
+import random
+
+# Asking The user name in te Reo 
+#is a Python quiz program to assist the user in learning Te Reo they are a total of 13 questions to be asked 
+score = 0
+
+#Question 1
+english = ["New Zealand", "Hungry", "Auckland", "Love", "One", "Good Night", "Tribe", "Family", "Good Morning", "Goodbye", "Canoe"]
+right_answer = ["Aotearoa", "Hiakai", "Tāmaki Makaurau", "Aroha", "tahi", "pō mārie", "Iwi", "Whānau", "Mōrena", "Ka kite anō", "Waka"]
+option_1 = ["Tāmaki Makaurau", "Kai", "Aotearoa", "Whānau", "wha", "Kia ora", "Whānau", "tahi", "Kia ora", "Kia ora", "Tainui"]
+option_2 = ["Te Whanganui-a-Tara", "Whāngai", "tauranga", "Tauoko", "rua", "Tēnā koe", "Aroha", "Aroha", "Kei te pai", "Kei te pai", "Aotea"]
+option_3 = ["Christchurch", "Hākari", "Christchurch", "Awhi", "toru", "haere mai", "tahi", "tahi", "Tino", "Mōrena", "Ka kite anō"]
+def generate_question(english,right_answer,option_1,option_2,option_3):
+  global score
+  print("What is the correct word for",english, "in maori")
+  random_sequence = random.randint(1,4)
+  if(random_sequence == 1):
+    print("A", option_1)
+    print("B", option_2)
+    print("C", right_answer)
+    print("D", option_3)
+    answer = input().lower()
+    if answer == "c":
+      print("Correct!")
+      score += 1
+    else:
+      print("incorrect")
+
+
+  elif(random_sequence == 2):
+    print("A", option_1)
+    print("B", right_answer)
+    print("C", option_2)
+    print("D", option_3)
+    answer = input().lower()
+    if answer == "a":
+      print("Correct!")
+      score += 1
+    else:
+      print("incorrect.")
+
+
+
+  elif(random_sequence == 3):
+    print("A", option_3)
+    print("B", right_answer)
+    print("C", option_1)
+    print("D", option_2)
+    answer = input().lower()
+    if answer == "b":
+      print("Correct!")
+      score += 1
+    else:
+      print("incorrect.")
+      
+
+
+  elif(random_sequence == 4):
+    print("A", option_3)
+    print("B", option_1)
+    print("C", option_2)
+    print("D", right_answer)
+    answer = input().lower()
+    if answer == "d":
+      print("Correct!")
+      score += 1
+    else:
+      print("incorrect.")
+      
+
+for m in range (0,11):
+ generate_question(english[m],right_answer[m],option_1[m],option_2[m],option_3[m])
+
+if score <= 1:
+  print("Your total score is:", score, "- better luck next time")
+elif score == 2:
+  print("you total score is:", score, "- you went ok.")
+elif score == 3:
+  print("your total score is:", score, "- good luck")
+else:
+  print("your total score is:", score, "- excellent")  
+
